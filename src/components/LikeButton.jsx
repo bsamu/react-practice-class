18a7b102ca7b899f50252b023ebad5a50da9e0c1
bsamu <bsamuel0220@gmail.com>
2022-02-23 20:24:27 +0100

    class extend

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+/*
 const LikeButton = () => {
+    const [isLiked, setIsLiked] = useState(false)
+
   return (
-    <div>LikeButton</div>
+    <div>
+        {!isLiked && <button onClick={() => setIsLiked(true)}>Like</button>}
+        {isLiked && <p>Liked</p>}
+    </div>
   )
+}
+*/
+class LikeButton extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLiked: false
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {!this.state.isLiked && <button onClick={() => this.setState({isLiked: true})}>Like</button>}
+                {this.state.isLiked && <p>Liked</p>}
+            </div>
+          )
+    }
 }
 
 export default LikeButton
